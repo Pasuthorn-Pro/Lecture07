71e96d3a72e7b9a9d1b13473b683503c4b2ae934
Pasuuu-Yahoo <pasuthorn1406@gmail.com>
2024-08-15 04:54:56 +0700

    Lecture07

---
+++ (pg.33)Creating and Accessing Dictionaries.py
@@ -0,0 +1,11 @@
+#Using {}
+student1 = {"name" : "Alice", "Age" : 25, "Grade" : "A"}
+print(student1)
+
+#Using dict() with keywords args
+student2 = dict(name="Bob", age=25, Grade="A")
+print(student2)
+
+#Using dict() with a list of tuples
+student3 = dict([("name", "David"), ("Age", 25), ("Grade", "A")])
+print(student3)
